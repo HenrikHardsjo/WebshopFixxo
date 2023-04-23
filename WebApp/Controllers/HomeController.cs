c53c91d99c6ffe0b0702f7998e5b1ccb70937a00
HenrikDesktop <heha98@live.se>
2023-04-23 20:46:20 +0200

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace _01_AspNetMVC.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        using var client = new HttpClient();
+        var result = await client.GetFromJsonAsync<IEnumerable<ProductModel>>("https://localhost:7049/api/products");
+
+        return View(result);
+    }
+}
